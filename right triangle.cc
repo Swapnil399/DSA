da512670c6f2af48400a0cad6f49300a7bced706
Swapnil399 <89090648+Swapnil399@users.noreply.github.com>
2022-01-11 23:30:23 +0530

    Create right triangle.cc

---
+++ right triangle.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+    int row_size;
+    cout << "Enter the row size:";
+    cin >> row_size;
+
+    for (int out = 1; out <= row_size; out++)
+    {
+        for (int in = 1; in <= out; in++)
+            cout << "*";
+            
+        cout << "\n";
+    }
+}
